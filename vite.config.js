718bd19deb9bb3560f55683437d0e565baea6bee
febrian-ssd <febrianaan@gmail.com>
2025-06-15 11:50:23 +0700

    Finalize assets for production

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import path from 'path'; // <-- Import path
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.scss', // <-- Ubah dari .css ke .scss
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
-        tailwindcss(),
     ],
+    // === TAMBAHKAN BAGIAN INI ===
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    },
+    // ============================
 });
