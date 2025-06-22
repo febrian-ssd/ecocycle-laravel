dd4b51ced32c509084d7e9bbd3be493b79c8a9db
febrian-ssd <febrianaan@gmail.com>
2025-06-22 18:26:05 +0700

    edit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -190,8 +190,9 @@
                 });
             }
         });
+        @yield('scripts')
     </script>
-    @yield('scripts')
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
