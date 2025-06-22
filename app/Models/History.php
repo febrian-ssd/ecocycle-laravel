e9e12a2fc38f40a6f09d094258a21da6842c9b68
febrian-ssd <febrianaan@gmail.com>
2025-06-22 17:19:27 +0700

    edit

--- app/Models/History.php
+++ app/Models/History.php
@@ -2,9 +2,37 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class History extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'dropbox_id',
+        'status',
+    ];
+
+    /**
+     * Mendefinisikan relasi: Satu History dimiliki oleh satu User.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Mendefinisikan relasi: Satu History terjadi di satu Dropbox.
+     */
+    public function dropbox()
+    {
+        return $this->belongsTo(Dropbox::class);
+    }
 }
