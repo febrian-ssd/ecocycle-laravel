7aa25876e797f109922c63e18b68be00effea8ef
febrian-ssd <febrianaan@gmail.com>
2025-06-22 21:51:29 +0700

    edit

--- routes/web.php
+++ routes/web.php
@@ -8,31 +8,71 @@ use App\Http\Controllers\Admin\DropboxController;
 use App\Http\Controllers\Admin\HistoryController;
 use App\Http\Controllers\Admin\SaldoController;
 
+/*
+|--------------------------------------------------------------------------
+| Authentication Routes
+|--------------------------------------------------------------------------
+*/
 Auth::routes();
 
+/*
+|--------------------------------------------------------------------------
+| Public Routes
+|--------------------------------------------------------------------------
+*/
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
+/*
+|--------------------------------------------------------------------------
+| Protected Routes - Authenticated Users
+|--------------------------------------------------------------------------
+*/
+Route::middleware(['auth'])->group(function () {
+    // User home page
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Admin Routes - Admin Only
+|--------------------------------------------------------------------------
+*/
 Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
+
+    // Admin Dashboard Redirect
     Route::redirect('/', '/admin/users');
 
-    // User Management
-    Route::get('/users', [UserController::class, 'index'])->name('users.index');
-    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
-    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy'); // <-- RUTE BARU
+    // User Management Routes
+    Route::prefix('users')->name('users.')->group(function () {
+        Route::get('/', [UserController::class, 'index'])->name('index');
+        Route::get('/{user}/edit', [UserController::class, 'edit'])->name('edit');
+        Route::put('/{user}', [UserController::class, 'update'])->name('update');
+        Route::delete('/{user}', [UserController::class, 'destroy'])->name('destroy');
+    });
 
-    // Dropbox Management
-    Route::get('/dropboxes', [DropboxController::class, 'index'])->name('dropboxes.index');
-    Route::get('/dropboxes/create', [DropboxController::class, 'create'])->name('dropboxes.create');
-    Route::post('/dropboxes', [DropboxController::class, 'store'])->name('dropboxes.store');
-    Route::get('/dropboxes/{dropbox}/edit', [DropboxController::class, 'edit'])->name('dropboxes.edit');
-    Route::put('/dropboxes/{dropbox}', [DropboxController::class, 'update'])->name('dropboxes.update');
+    // Dropbox Management Routes
+    Route::prefix('dropboxes')->name('dropboxes.')->group(function () {
+        Route::get('/', [DropboxController::class, 'index'])->name('index');
+        Route::get('/create', [DropboxController::class, 'create'])->name('create');
+        Route::post('/', [DropboxController::class, 'store'])->name('store');
+        Route::get('/{dropbox}/edit', [DropboxController::class, 'edit'])->name('edit');
+        Route::put('/{dropbox}', [DropboxController::class, 'update'])->name('update');
+        Route::delete('/{dropbox}', [DropboxController::class, 'destroy'])->name('destroy'); // <- ROUTE YANG HILANG
+        Route::get('/{dropbox}', [DropboxController::class, 'show'])->name('show'); // Optional: untuk view detail
+    });
 
-    // History
-    Route::get('/history', [HistoryController::class, 'index'])->name('history.index');
+    // History Management Routes
+    Route::prefix('history')->name('history.')->group(function () {
+        Route::get('/', [HistoryController::class, 'index'])->name('index');
+        Route::get('/{history}', [HistoryController::class, 'show'])->name('show'); // Optional: untuk view detail
+        Route::delete('/{history}', [HistoryController::class, 'destroy'])->name('destroy'); // Optional: untuk delete history
+    });
 
-    // Saldo Management
-    Route::get('/saldo/topup', [SaldoController::class, 'topupIndex'])->name('saldo.topup.index');
-    Route::post('/saldo/topup/{topupRequest}/approve', [SaldoController::class, 'approveTopup'])->name('saldo.topup.approve');
-    Route::delete('/admin/dropboxes/{dropbox}', [DropboxController::class, 'destroy'])->name('admin.dropboxes.destroy');
+    // Saldo Management Routes
+    Route::prefix('saldo')->name('saldo.')->group(function () {
+        Route::prefix('topup')->name('topup.')->group(function () {
+            Route::get('/', [SaldoController::class, 'topupIndex'])->name('index');
+            Route::post('/{topupRequest}/approve', [SaldoController::class, 'approveTopup'])->name('approve');
+        });
+    });
 });
